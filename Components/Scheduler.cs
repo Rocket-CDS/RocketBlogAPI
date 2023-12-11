cbf45b544ae985a833cb4930b99dc62916496aae
leedavi <dcl@agence-sesame.fr>
2023-12-11 01:26:59 +0100

    Events + Scheduler for autopublish

--- Components/Scheduler.cs
+++ Components/Scheduler.cs
@@ -24,7 +24,22 @@ namespace RocketBlogAPI.Components
 
                 if (portalCatalog.Active && (portalCatalog.SchedulerRunHours == 0 || (portalCatalog.LastSchedulerTime < DateTime.Now.AddHours(portalCatalog.SchedulerRunHours * -1))))
                 {
-
+                    var objCtrl = new DNNrocketController();
+                    
+                    // Publish hidden articles on publishdate.
+                    var l = objCtrl.GetList(portalId, -1, "rocketblogapiART", "and [XMLData].value('(genxml/checkbox/hidden)[1]','bit') = 1 and [XMLData].value('(genxml/checkbox/autopublish)[1]','bit') = 1", "","",0,0,0,0, "RocketDirectoryAPI");
+                    foreach (var sInfo in l)
+                    {
+                        var articleData = new ArticleLimpet(sInfo.ItemID, DNNrocketUtils.GetCurrentCulture(), "rocketblogapi");
+                        if (articleData.Exists)
+                        {
+                            if (articleData.Info.GetXmlPropertyDate("genxml/textbox/publisheddate").Date <= DateTime.Now.Date)
+                            {
+                                articleData.Info.SetXmlProperty("genxml/checkbox/hidden", "false");
+                                articleData.Update();
+                            }
+                        }
+                    }
                     portalCatalog.LastSchedulerTime = DateTime.Now;
                     portalCatalog.Update();
                 }
