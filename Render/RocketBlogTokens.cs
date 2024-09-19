79aac4eea11670aa74d139c00bdace2f6904cc74
David Lee <dcl@agence-sesame.fr>
2024-09-19 12:46:11 +0200

    AssignDataModel

--- Render/RocketBlogTokens.cs
+++ Render/RocketBlogTokens.cs
@@ -1,5 +1,6 @@
 using DNNrocketAPI.Components;
 using RazorEngine.Text;
+using RocketDirectoryAPI.Components;
 using Simplisity;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,22 @@ using System.Text;
 
 namespace RocketBlogAPI.Components
 {
-    public class RocketBlogAPITokens<T> : DNNrocketAPI.render.DNNrocketTokens<T>
+    public class RocketBlogAPITokens<T> : RocketDirectoryAPI.Components.RocketDirectoryAPITokens<T>
     {
 
+        public DateTime monthStartDate;
+        public DateTime monthEndDate;
+        public DateTime calMonthStartDate;
+        public DateTime articleEventStartDate;
+        public DateTime articleEventEndDate;
+        public string[] listUrlParams;
+        public new string AssignDataModel(SimplisityRazor sModel)
+        {
+            base.AssignDataModel(sModel);
+
+
+            return "";
+        }
 
     }
 }
